ee68b4d6de9be19b911f51a08dae24ee89cc9a84
Jon St John <jonstjohn@gmail.com>
2013-06-05 17:40:16 -0600

    Todo completed button

--- main.js
+++ main.js
@@ -13,4 +13,17 @@ function TodoCtrl($scope) {
         $scope.todos.push( {text: $scope.formTodoText, done: false});
         $scope.formTodoText = "";
     };
+
+    $scope.clearCompleted = function() {
+        var todos = [];
+        for (var i = 0; i < $scope.todos.length; i++) {
+            if (!$scope.todos[i].done) {
+                todos.push($scope.todos[i]);
+            }
+        }
+        $scope.todos = todos;
+        $scope.todos = $scope.filter($scope.todos, function(todo) {
+            return !todo.done;
+        });
+    }
 }
