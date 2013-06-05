882f2a209cae5318276090f1529bee83f36bc05b
Jon St John <jonstjohn@gmail.com>
2013-06-05 17:24:54 -0600

    Todo checkbox list

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function TodoCtrl($scope) {
     $scope.totalTodos = 4;
+
+    $scope.todos = [
+        { text: 'Learn AngularJS', done: false},
+        {text: 'Build an app', done: false}
+    ];
 }
