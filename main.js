15beecf873eadfb1c4a9330ee13da74a45d1607a
Jon St John <jonstjohn@gmail.com>
2013-06-05 17:32:57 -0600

    Todo - add todo

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function TodoCtrl($scope) {
-    $scope.totalTodos = 4;
 
     $scope.todos = [
         { text: 'Learn AngularJS', done: false},
         {text: 'Build an app', done: false}
     ];
+
+    $scope.getTotalTodos = function() {
+        return $scope.todos.length;
+    }
+
+    $scope.addTodo = function() {
+        $scope.todos.push( {text: $scope.formTodoText, done: false});
+        $scope.formTodoText = "";
+    };
 }
