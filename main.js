58587a33325db8fe208e8cd63718745246b64bfb
Nguyen Hoang Duy Tan <tan@drivecurrent.com>
2013-06-06 17:29:12 -0600

    basic controller and binding

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-function TodoCtrl($scope) {
+var app = angular.module('app', []);
 
-    $scope.todos = [
-        { text: 'Learn AngularJS', done: false},
-        {text: 'Build an app', done: false}
-    ];
+app.factory('todos', function(){
+	return {data:
+		[
+			{text: 'Learn AngularJS : controller', done: false},
+			{text: 'Learn AngularJS : view', done: false},
+			{text: 'Learn AngularJS : model', done: false},
+			{text: 'Learn AngularJS : router', done: false},
+			{text: 'Learn AngularJS : provider', done: false},
+			{text: 'Learn AngularJS : data binding', done: false},
+			{text: 'Learn AngularJS : data filter', done: false},
+			{text: 'Learn AngularJS : data scope', done: false},
+			{text: 'Learn AngularJS : data module', done: false},
+			{text: 'Learn AngularJS : denpendency injector', done: false},
+			{text: 'Learn AngularJS : directives', done: false},
+			{text: 'Learn AngularJS : promisses', done: false},
+			{text: 'Learn AngularJS : services', done: false},
+			{text: 'Learn AngularJS : testing', done: false}
+		]
+	};	
+});
 
+
+function TodoCtrl($scope, todos) {
+    $scope.todos = todos.data;
+	$scope.completes = [];
     $scope.getTotalTodos = function() {
         return $scope.todos.length;
     }
-
+	
     $scope.addTodo = function() {
         $scope.todos.push( {text: $scope.formTodoText, done: false});
         $scope.formTodoText = "";
     };
 
     $scope.clearCompleted = function() {
-        var todos = [];
-        for (var i = 0; i < $scope.todos.length; i++) {
+        var todoss = [];
+        for (var i = 0; i < $scope.todos.length; i++) {			
             if (!$scope.todos[i].done) {
-                todos.push($scope.todos[i]);
-            }
+                todoss.push($scope.todos[i]);
+            } else {
+				$scope.completes.push($scope.todos[i]);
+			}
         }
-        $scope.todos = todos;
-        $scope.todos = $scope.filter($scope.todos, function(todo) {
-            return !todo.done;
-        });
+        $scope.todos = todoss;
+		todos.data   = todoss;
     }
+}
+
+function SearchToDoCtrl($scope, todos) {	
+	$scope.todos = todos.data;
+	$scope.searchToDo = function() {
+		$scope.todos = todos.data;		
+	}
 }
